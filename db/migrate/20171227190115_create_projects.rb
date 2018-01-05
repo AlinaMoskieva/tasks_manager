19f2e89f240b1859e586b5732e837125ab37fadd
Alina Moskieva <alina.moskieva@flatstack.com>
2018-01-05 19:56:29 +0300

    authenticate before action and add user to project

--- db/migrate/20171227190115_create_projects.rb
+++ db/migrate/20171227190115_create_projects.rb
@@ -2,6 +2,7 @@ class CreateProjects < ActiveRecord::Migration[5.1]
   def change
     create_table :projects do |t|
       t.string :title, null: false
+      t.belongs_to :user, null: false
 
       t.timestamps
     end
--- db/schema.rb
+++ db/schema.rb
@@ -17,8 +17,10 @@ ActiveRecord::Schema.define(version: 20171227190115) do
 
   create_table "projects", force: :cascade do |t|
     t.string "title", null: false
+    t.bigint "user_id", null: false
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
+    t.index ["user_id"], name: "index_projects_on_user_id"
   end
 
   create_table "users", force: :cascade do |t|
