e73693748e62a9724e2aea07ae6b06b06a86c9a9
Alina Moskieva <alina.moskieva@flatstack.com>
2018-01-07 16:15:19 +0300

    Add ability to get token

--- config/routes.rb
+++ config/routes.rb
@@ -1,9 +1,17 @@
 Rails.application.routes.draw do
   mount RailsAdmin::Engine => "/admin", as: "rails_admin"
+
+  resources :projects
   resources :tasks do
     resources :comments, shallow: true
   end
-  resources :projects
+
+  namespace :api do
+    namespace :v1 do
+      resources :tasks
+      resources :tokens, only: %i(create)
+    end
+  end
 
   devise_for :users
 
